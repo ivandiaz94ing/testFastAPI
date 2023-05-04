587811663c825f1c5bb1c2895e585c4a02ae2dfb
Ivan Diaz <ivandiaz94ing@gmail.com>
2023-05-04 17:15:35 -0500

    Creando pruebas con FastAPI

--- test_pytest.py
+++ test_pytest.py
@@ -6,3 +6,31 @@ app = FastAPI()
 
 def test_primera():
     assert masseis(8)==14
+
+from typing import Union
+
+from fastapi import FastAPI
+
+
+@app.get("/")   
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hello")   
+def hello():
+    return {"Hello": "FastAPI"}
+
+@app.get("/IsPrime")   
+def esPrimo(numero):
+    if numero < 2:
+        return "False"
+    for i in range(2, numero):
+        if numero % i == 0:
+            return "False"
+    return "True"
+
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
